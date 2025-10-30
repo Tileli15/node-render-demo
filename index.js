422d053c13d70624c65567eed24bd46f21336914
thileli <tthileli285@gmail.com>
2025-10-30 08:53:16 +0100

    Initialisation du projet Node.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Bonjour depuis Render !'));
+app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
